ac444c8450ed5873acd516ad6a4e83ee967d7710
Peter de Lange <lange@dbis.rwth-aachen.de>
2020-10-15 14:52:52 +0200

    adjust to new backend
    and package updates

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ require('dotenv').config();
 const MQTT = require('mqtt');
 const { GraphQLClient } = require('graphql-request');
 
-
 const userMail = process.env.USERMAIL;
 const userName = process.env.USERNAME;
 const userPw = process.env.USERPW;
 const backendLocation = process.env.BACKENDLOCATION;
 
 var mqttClient = MQTT.connect(process.env.MQTTSERVERADDRESS, {
-  clientId: 'braustube-mqttProxy',
+  clientId: 'braustube-mqttProxy'
 });
 
 var graphQLClient = new GraphQLClient(backendLocation);
@@ -29,7 +28,7 @@ async function main() {
   `;
   let variables = {
     email: userMail,
-    password: userPw,
+    password: userPw
   };
   try {
     data = await graphQLClient.request(mutation, variables);
@@ -48,7 +47,7 @@ async function main() {
     variables = {
       name: userName,
       password: userPw,
-      email: userMail,
+      email: userMail
     };
     try {
       data = await graphQLClient.request(mutation, variables);
@@ -64,8 +63,8 @@ async function main() {
   // update client variable
   graphQLClient = new GraphQLClient(backendLocation, {
     headers: {
-      Authorization: 'Bearer ' + token,
-    },
+      Authorization: 'Bearer ' + token
+    }
   });
   console.log('Starting...');
 
@@ -86,21 +85,21 @@ mqttClient.on('message', function (topic, message) {
   let sensorTimeStamp = new Date().toJSON();
 
   let sensorData = {
-    sensorName: sensorName,
-    sensorTimeStamp: sensorTimeStamp,
-    sensorValue: sensorValue,
+    sensor_name: sensorName,
+    sensor_time_stamp: sensorTimeStamp,
+    sensor_value: sensorValue
   };
   try {
     const mutation = /* GraphQL */ `
       mutation addGraphData(
-        $sensorName: String!
-        $sensorTimeStamp: DateTime!
-        $sensorValue: String!
+        $sensor_name: String!
+        $sensor_time_stamp: DateTime!
+        $sensor_value: String!
       ) {
         addGraphData(
-          sensorName: $sensorName
-          sensorTimeStamp: $sensorTimeStamp
-          sensorValue: $sensorValue
+          sensor_name: $sensor_name
+          sensor_time_stamp: $sensor_time_stamp
+          sensor_value: $sensor_value
         ) {
           id
         }
@@ -108,9 +107,15 @@ mqttClient.on('message', function (topic, message) {
     `;
     graphQLClient
       .request(mutation, sensorData)
-      .then((data) => console.log('Stored data for ' + sensorName + 'with id: ' + data.addGraphData.id))
+      .then((data) =>
+        console.log(
+          'Stored data for ' + sensorName + 'with id: ' + data.addGraphData.id
+        )
+      )
       .catch((error) =>
-        console.error(JSON.parse(JSON.stringify(error)).response.errors[0].message)
+        console.error(
+          JSON.parse(JSON.stringify(error)).response.errors[0].message
+        )
       );
   } catch (e) {
     console.log(e);
